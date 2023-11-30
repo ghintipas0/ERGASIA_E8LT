a086733a71fd9c0f4bbed84c4e22174e6e015b09
Christos Sampakidis <christossabakidi@gmail.com>
2023-11-30 17:34:02 +0200

    Merging backend with react (step 1)

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -4,14 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div className="App">
-        <header className="App-header">
-          <p> CODE HERE</p>
-        </header>
-     </div>
-);
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+
+//);
+
+ var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+
+
+var slideIndexSecond = 1;
+showSlidesSecond(slideIndexSecond);
+
+function plusSlidesSecond(n) {
+  showSlidesSecond(slideIndexSecond += n);
+}
+
+function showSlidesSecond(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-second");
+  if (n > slides.length) {slideIndexSecond = 1}
+  if (n < 1) {slideIndexSecond = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndexSecond-1].style.display = "block";
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
