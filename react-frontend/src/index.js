2728b85be119d64851aa26d8caaadcb85855f7e5
gsoti <gsotis2001@gmail.com>
2023-12-04 15:37:04 +0200

    test commit

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -10,41 +10,41 @@ import reportWebVitals from './reportWebVitals';
 //);
 
  var slideIndex = 1;
-  showSlides(slideIndex);
+   showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+   function plusSlides(n) {
+     showSlides(slideIndex += n);
+   }
 
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-  }
+   function showSlides(n) {
+     var i;
+     var slides = document.getElementsByClassName("mySlides");
+     if (n > slides.length) {slideIndex = 1}
+     if (n < 1) {slideIndex = slides.length}
+     for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+     }
+     slides[slideIndex-1].style.display = "block";
+   }
 
 
-var slideIndexSecond = 1;
-showSlidesSecond(slideIndexSecond);
+ var slideIndexSecond = 1;
+ showSlidesSecond(slideIndexSecond);
 
-function plusSlidesSecond(n) {
-  showSlidesSecond(slideIndexSecond += n);
-}
+ function plusSlidesSecond(n) {
+   showSlidesSecond(slideIndexSecond += n);
+ }
 
-function showSlidesSecond(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides-second");
-  if (n > slides.length) {slideIndexSecond = 1}
-  if (n < 1) {slideIndexSecond = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndexSecond-1].style.display = "block";
-}
+ function showSlidesSecond(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides-second");
+   if (n > slides.length) {slideIndexSecond = 1}
+   if (n < 1) {slideIndexSecond = slides.length}
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   slides[slideIndexSecond-1].style.display = "block";
+ }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
