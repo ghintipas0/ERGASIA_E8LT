3ef8a1cd3116f86073932da333dcbd43a417a076
gsoti <gsotis2001@gmail.com>
2023-12-04 16:14:36 +0200

    test commit

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -9,42 +9,38 @@ import reportWebVitals from './reportWebVitals';
 
 //);
 
- var slideIndex = 1;
-   showSlides(slideIndex);
+ var slideIndex = 0;
 
-   function plusSlides(n) {
-     showSlides(slideIndex += n);
-   }
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
-   function showSlides(n) {
-     var i;
-     var slides = document.getElementsByClassName("mySlides");
-     if (n > slides.length) {slideIndex = 1}
-     if (n < 1) {slideIndex = slides.length}
-     for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-     }
-     slides[slideIndex-1].style.display = "block";
-   }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
 
+    slideIndex++;
 
- var slideIndexSecond = 1;
- showSlidesSecond(slideIndexSecond);
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
 
- function plusSlidesSecond(n) {
-   showSlidesSecond(slideIndexSecond += n);
- }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
 
- function showSlidesSecond(n) {
-   var i;
-   var slides = document.getElementsByClassName("mySlides-second");
-   if (n > slides.length) {slideIndexSecond = 1}
-   if (n < 1) {slideIndexSecond = slides.length}
-   for (i = 0; i < slides.length; i++) {
-     slides[i].style.display = "none";
-   }
-   slides[slideIndexSecond-1].style.display = "block";
- }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Εμφάνιση του επόμενου slide κάθε 4 δευτερόλεπτα
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  // Έναρξη της παρουσίασης των slides
+  showSlides();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
