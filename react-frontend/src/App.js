3c4d577df5267d6602ed058b002aec0b41b13a00
Christos Sampakidis <christossabakidi@gmail.com>
2023-12-07 19:47:31 +0200

    testing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from "./Pages/Home";
+import {Route,Link} from 'react-router';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> TEST2</p>
-      </header>
-    </div>
-  );
+  return <div className="App">
+      <Route exact to="/" component ={Home} />
+  </div>
 }
 
 export default App;
