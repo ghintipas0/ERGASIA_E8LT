ca8ec80573d0e29f53968d63ede29038655afd38
Christos Sampakidis <christossabakidi@gmail.com>
2023-12-07 20:40:47 +0200

    Converted to React

--- react-frontend/src/Pages/Home.js
+++ react-frontend/src/Pages/Home.js
@@ -1,98 +1,120 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../component/NavBar";
 
 function Home() {
-  return (
-    <div>
-      <div className="search-container">
-        <input type="text" placeholder="Ψάχνεις για..." className="search-input" />
-      </div>
+    const [slideIndex, setSlideIndex] = useState(0);
 
-      <div className="container">
-        <button className="rotate-on-hover">
-          <img src="Media/wishlist.png" alt="Wishlist Icon" />
-        </button>
-        <button className="rotate-on-hover special-button">
-          <img src="Media/profile.png" alt="Profile Icon" />
-          <div className="tooltip" id="tooltip">
-            Συνδέσου τώρα
-          </div>
-        </button>
-        <button className="rotate-on-hover">
-          <img src="Media/cart.png" alt="Cart Icon" />
-        </button>
-      </div>
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            showSlides();
+        }, 3000);
 
-      <div className="button-container">
-        <button className="button">
-          <img src="Media/laptop.png" alt="Image" />
-          PC & Laptops
-        </button>
-        <button className="button">
-          <img src="Media/phones.png" alt="Image" />
-          Smartphones
-        </button>
-        <button className="button">
-          <img src="Media/tv.png" alt="Image" />
-          Τηλεοράσεις
-        </button>
-      </div>
+        return () => clearInterval(intervalId);
+    }, []);
 
-      {/* First Slideshow */}
-      <div className="slideshow-container">
-        {/* Your slideshow content here */}
-      </div>
-
-      {/* Second Slideshow */}
-      <div className="slideshow-container-second">
-        {/* Your second slideshow content here */}
-        <div className="text-second">Top Offers</div>
-      </div>
-    </div>
-  );
-}
-
-var slideIndex = 0;
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    function showSlides() {
+        setSlideIndex((prevIndex) => {
+            let newIndex = prevIndex + 1;
+            if (newIndex > 3) {
+                newIndex = 1;
+            }
+            return newIndex;
+        });
     }
 
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
+    function currentSlide(n) {
+        setSlideIndex(n);
     }
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    function showTooltip() {
+        // Use React state to handle tooltip visibility
     }
 
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 3000); // Εμφάνιση του επόμενου slide κάθε 4 δευτερόλεπτα
+    function hideTooltip() {
+        // Use React state to handle tooltip visibility
+    }
+
+    return (
+        <div>
+            <div className="search-container">
+                <input type="text" placeholder="Ψάχνεις για..." className="search-input" />
+            </div>
+
+            <div className="container">
+                <button className="rotate-on-hover">
+                    <img src="Media/wishlist.png" alt="Wishlist Icon" />
+                </button>
+                <button
+                    className="rotate-on-hover special-button"
+                    onMouseOver={showTooltip}
+                    onMouseOut={hideTooltip}
+                >
+                    <img src="Media/profile.png" alt="Profile Icon" />
+                    <div className="tooltip" id="tooltip">
+                        Συνδέσου τώρα
+                    </div>
+                </button>
+
+                <button className="rotate-on-hover">
+                    <img src="Media/cart.png" alt="Cart Icon" />
+                </button>
+            </div>
+
+            <div className="button-container">
+                <button className="button">
+                    <img src="Media/laptop.png" alt="Image" />
+                    PC & Laptops
+                </button>
+                <button className="button">
+                    <img src="Media/phones.png" alt="Image" />
+                    Smartphones
+                </button>
+                <button className="button">
+                    <img src="Media/tv.png" alt="Image" />
+                    Τηλεοράσεις
+                </button>
+            </div>
+
+            <div className="slideshow-container">
+                <div className="mySlides">
+                    <img src="Media/lenovoidea.png" alt="Περιγραφή φωτογραφίας" style={{ width: "300px", height: "300px", borderRadius: "15px", position: "absolute", left: "50px", zIndex: "1" }} />
+                    <div className="text" style={{ fontSize: "18px", top: "90px", left: "160px", fontWeight: "bold" }}>Lenovo IdeaPad Gaming 3 15IHU6 15.6" IPS FHD</div>
+                    <div className="text" style={{ fontSize: "28px", top: "130px", left: "160px", fontWeight: "bold" }}>648,99€</div>
+                    <div className="content" style={{ width: "800px", height: "300px", backgroundColor: "#f7f7f7", margin: "auto", borderRadius: "15px" }}></div>
+                </div>
+
+                <div className="mySlides">
+                    <div className="content" style={{ width: "800px", height: "300px", backgroundColor: "#f7f7f7", margin: "auto", borderRadius: "15px" }}></div>
+                </div>
+
+                <div className="mySlides">
+                    <div style={{ width: "800px", height: "300px", backgroundColor: "#f7f7f7", margin: "auto", borderRadius: "15px" }}></div>
+                </div>
+
+                <div style={{ textAlign: "center" }}>
+                    <span className="dot" onClick={() => currentSlide(1)}></span>
+                    <span className="dot" onClick={() => currentSlide(2)}></span>
+                    <span className="dot" onClick={() => currentSlide(3)}></span>
+                </div>
+            </div>
+
+            <div className="slideshow-container-second">
+                <div className="mySlides-second">
+                    <div style={{ width: "800px", height: "300px", backgroundColor: "#f7f7f7", margin: "auto", borderRadius: "15px" }}></div>
+                </div>
+
+                <div className="mySlides-second">
+                    <div style={{ width: "800px", height: "300px", backgroundColor: "#f7f7f7", margin: "auto", borderRadius: "15px" }}></div>
+                </div>
+
+                <div className="mySlides-second">
+                    <div style={{ width: "800px", height: "300px", backgroundColor: "#f7f7f7", margin: "auto", borderRadius: "15px" }}></div>
+                </div>
+
+                <div className="text-second">Top Offers</div>
+            </div>
+        </div>
+    );
 }
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-// Έναρξη της παρουσίασης των slides
-showSlides();
-
-
-function showTooltip() {
-    document.getElementById("tooltip").style.display = "block";
-}
-
-function hideTooltip() {
-    document.getElementById("tooltip").style.display = "none";
-}
-
 
 export default Home;
