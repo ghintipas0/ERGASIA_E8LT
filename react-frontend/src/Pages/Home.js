b3d277d2e21a48c3f95e0d8f68301ff36b2ac4d8
Christos Sampakidis <christossabakidi@gmail.com>
2023-12-07 20:27:00 +0200

    testing 2

--- react-frontend/src/Pages/Home.js
+++ react-frontend/src/Pages/Home.js
@@ -1,21 +1,8 @@
 import React from "react";
-
+import NavBar from "../component/NavBar";
 function Home() {
     return(
         <div>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li>
-                    <a href="#Κατηγορίες">Κατηγορίες</a>
-                    <div className="subcategories">
-                        <a href="#pc-laptops" className="subcategory">PC & Laptops</a>
-                        <a href="#smartphones" className="subcategory">Smartphones</a>
-                        <a href="#tv" className="subcategory">Τηλεοράσεις</a>
-                    </div>
-                </li>
-                <li><a href="#Προιόντα">Προιόντα</a></li>
-            </ul>
-
             <div className="search-container">
                 <input type="text" placeholder="Ψάχνεις για..." className="search-input" />
             </div>
@@ -24,8 +11,7 @@ function Home() {
                 <button className="rotate-on-hover"><img src="Media/wishlist.png" alt="Wishlist Icon" /></button>
                 <button
                     className="rotate-on-hover special-button"
-                    onMouseOver={showTooltip}
-                    onMouseOut={hideTooltip}
+
                 >
                     <img src="Media/profile.png" alt="Profile Icon" />
                     <div className="tooltip" id="tooltip">Συνδέσου τώρα</div>
@@ -33,9 +19,6 @@ function Home() {
 
                 <button className="rotate-on-hover"><img src="Media/cart.png" alt="Cart Icon" /></button>
             </div>
-
-            {/* ... Rest of your code ... */}
-
         </div>
     );
 }
