ca9146acfd9f4751884be3f058b61ee57458a67e
Christos Sampakidis <christossabakidi@gmail.com>
2023-12-07 20:35:32 +0200

    testing 3

--- react-frontend/src/Pages/Home.js
+++ react-frontend/src/Pages/Home.js
@@ -52,4 +52,47 @@ function Home() {
   );
 }
 
+var slideIndex = 0;
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Εμφάνιση του επόμενου slide κάθε 4 δευτερόλεπτα
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// Έναρξη της παρουσίασης των slides
+showSlides();
+
+
+function showTooltip() {
+    document.getElementById("tooltip").style.display = "block";
+}
+
+function hideTooltip() {
+    document.getElementById("tooltip").style.display = "none";
+}
+
+
 export default Home;
