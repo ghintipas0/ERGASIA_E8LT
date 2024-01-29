216a77be65079a375e3b313552f6512bec56dac6
ChristosSampakidis <christossabakidi@gmail.com>
2024-01-29 09:36:14 +0200

    login/register navbar validation hotfix

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './component/NavBar';
 import MyProfile from './Pages/MyProfile';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import Admin from './Pages/Admin';
 function App() {
     return (
             <div className="App">
@@ -13,6 +14,7 @@ function App() {
                      <Route exact path="/MyProfile" element={<MyProfile />} />
                     <Route exact path="/Login" element={<Login />} />
                     <Route exact path="/Register" element={<Register/>} />
+                    <Route exact path="/Admin" element={<Admin/>} />
                 </Routes>
             </div>
     );
---
+++ react-frontend/src/Pages/Admin.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+function UserAuth() {
+    return (
+      <div>
+          <div className="sticky top"
+               style={{background: "black", position: "absolute", display: "flex", height: "100%", width: "10vw"}}>
+              <div style={{display: "grid", gridTemplateColumns:"1",  width:"100%"}}>
+                  <button style={{width:"100%"}}>test</button>
+                  <button>test</button>
+                  <button>test</button>
+              </div>
+          </div>
+      </div>
+    );
+}
+
+export default UserAuth;
