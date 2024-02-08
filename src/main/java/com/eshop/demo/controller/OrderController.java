4b29262ba3959d33093c94b810876edfe27a0534
ChrisMi1 <xristosmalt@hotmail.com>
2024-02-08 19:45:52 +0200

    Delete user

---
+++ src/main/java/com/eshop/demo/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.eshop.demo.controller;
+
+import com.eshop.demo.model.OrderBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class OrderController {
+    @GetMapping("/order")
+    public void order(@RequestBody OrderBody orders ){
+        System.out.println(orders);
+    }
+}
