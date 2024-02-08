1f537e22fd66775120e7e84f08ae8cf44b98ccc8
ChristosSampakidis <christossabakidi@gmail.com>
2024-02-08 15:16:19 +0200

    Admin 50%

--- react-frontend/src/Pages/Admin.js
+++ react-frontend/src/Pages/Admin.js
@@ -1,25 +1,55 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import './Admin.css';
-
-
+import Admin_Add_User from "../component/Admin_Add_User";
+import $ from "jquery";
+import Admin_Add_Product from "../component/Admin_Add_Product";
 function Admin() {
-    function show_sub_page(id){
-
-    }
+    const [showFields, setShowFields] = useState({
+        add_user: true,
+        remove_user: false,
+        add_product: false,
+        remove_product: false
+    });
+    const toggleField = (field) => {
+        setShowFields((prev) => ({
+            ...Object.keys(prev).reduce((acc, key) => {
+                acc[key] = key === field;
+                return acc;
+            }, {})
+        }));
+    };
     return (
-      <div>
-          <div className="sticky_top"
-               style={{background: "black", position: "sticky", top:"8em", display: "flex", height: "5vh", width: "100%"}}>
-              <div style={{display: "grid", gridTemplateColumns: "auto auto auto auto",gridTemplateRows:"auto", width: "100%"}}>
-                  <button id="add_user" onClick={show_sub_page(this)}>Add user</button>
-                  <button id="remove_user" onClick={show_sub_page(this)}>Remove User</button>
-                  <button id="add_product" onClick={show_sub_page(this)}>Add Product</button>
-                  <button id="remove_product" onClick={show_sub_page(this)}>Remove Product</button>
-              </div>
+        <div>
+            <div className="sticky_top"
+                 style={{
+                     background: "black",
+                     position: "sticky",
+                     top: "8em",
+                     display: "flex",
+                     height: "5vh",
+                     width: "100%",
+                     zIndex: "2"
+                 }}>
+                <div style={{
+                    display: "grid",
+                    gridTemplateColumns: "auto auto auto auto",
+                    gridTemplateRows: "auto",
+                    width: "100%",
+                    zIndex: "2"
+                }}>
+                    <button onClick={() => toggleField("add_user")}>Add user</button>
+                    <button onClick={() => toggleField("remove_user")}>Remove User</button>
+                    <button onClick={() => toggleField("add_product")}>Add Product</button>
+                    <button onClick={() => toggleField("remove_product")}>Remove Product</button>
 
-          </div>
+                </div>
+            </div>
+            <div style={{marginTop:"2vh"}}>
+                {showFields.add_user && <Admin_Add_User />}
+                {showFields.add_product && <Admin_Add_Product />}
+            </div>
 
-      </div>
+        </div>
     );
 }
 
