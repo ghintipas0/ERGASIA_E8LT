ec6fda7bf75e04b64232ed38c03742d7b0c49de1
ChrisMi1 <xristosmalt@hotmail.com>
2024-03-16 15:10:08 +0200

    Dont insert payment details second time.

--- src/main/java/com/eshop/demo/DAO/PaymentDAO.java
+++ src/main/java/com/eshop/demo/DAO/PaymentDAO.java
@@ -3,5 +3,8 @@ package com.eshop.demo.DAO;
 import com.eshop.demo.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentDAO extends JpaRepository<Payment,Integer> {
+    Optional<Payment> findByCardNumber(String cardNumber);
 }
